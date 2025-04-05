2f1386dcafc5aac60ecbcc27f7250c4f73e23695
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2025-04-05 10:10:15 +0200

    feat: add revenue Data Analysis

---
+++ plot_settings.py
@@ -0,0 +1,8 @@
+# color palette from Tableau "Lightning Water"
+colors_primary = ["#2d9ced", "#68ceee", "#96f2ee", "#0e6ece", "#073e92", "#051c61"] 
+
+# color palette from Tableau "Lightning Sunrise"
+colors_secondary = ["#f59623", "#f5c062", "#f5de98", "#f59623", "#f59623", "#300561"] 
+
+# mixed color palette
+colors_mixed = ["#073e92", "#f5c062", "#68ceee", "#f59623", "#f59623"] 
